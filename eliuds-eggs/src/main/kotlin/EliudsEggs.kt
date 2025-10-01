59aca0f384a408618c1a1843b6643b21d12f9e48
Robchike9290 <robchike9290@gmail.com>
2025-10-01 11:39:47 -0500

    Solved another

---
+++ eliuds-eggs/src/main/kotlin/EliudsEggs.kt
@@ -0,0 +1,11 @@
+object EliudsEggs {
+
+    fun eggCount(number: Int): Int{
+        val binaryString = Integer.toBinaryString(number)
+        var numberOfEggs = 0
+        for (char in binaryString) {
+            if (char == '1') numberOfEggs++
+        }
+        return numberOfEggs
+    }
+}
