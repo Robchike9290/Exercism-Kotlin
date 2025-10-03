c54c20125274e223266b329a3ab705f9179b7a18
Robchike9290 <robchike9290@gmail.com>
2025-10-03 14:03:58 -0500

    Solve another

--- resistor-color/src/main/kotlin/ResistorColor.kt
+++ resistor-color/src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,24 @@
+enum class Color(val code: Int) {
+    BLACK(0),
+    BROWN(1),
+    RED(2),
+    ORANGE(3),
+    YELLOW(4),
+    GREEN(5),
+    BLUE(6),
+    VIOLET(7),
+    GREY(8),
+    WHITE(9);
+}
+
 object ResistorColor {
 
     fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
+        return Color.valueOf(input.uppercase()).code
     }
 
     fun colors(): List<String> {
-        TODO("Implement this to complete the task")
+        return Color.values().map { it.name.lowercase() }
     }
 
 }
--- resistor-color/src/test/kotlin/ResistorColorTest.kt
+++ resistor-color/src/test/kotlin/ResistorColorTest.kt
@@ -1,21 +1,17 @@
-import kotlin.test.Ignore
-import kotlin.test.assertEquals
-import kotlin.test.Test
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
 
 class ResistorColorTest {
 
     @Test
     fun testBlackColorCode() = assertEquals(0, ResistorColor.colorCode("black"))
 
-    @Ignore
     @Test
     fun testWhiteColorCode() = assertEquals(9, ResistorColor.colorCode("white"))
 
-    @Ignore
     @Test
     fun testOrangeColorCode() = assertEquals(3, ResistorColor.colorCode("orange"))
 
-    @Ignore
     @Test
     fun testColors() {
         val expected = listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
