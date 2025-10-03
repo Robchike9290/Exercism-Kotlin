a1d0c84c8175524cd0ed87c82a93558b6f218995
Robchike9290 <robchike9290@gmail.com>
2025-10-03 10:54:15 -0500

    Solve another

---
+++ perfect_numbers/src/main/kotlin/NaturalNumber.kt
@@ -0,0 +1,19 @@
+package perfect_numbers.src.main.kotlin
+
+
+enum class Classification {
+    DEFICIENT, PERFECT, ABUNDANT
+}
+
+object NaturalNumber {
+    fun classify(naturalNumber: Int): Classification {
+        require(naturalNumber > 0) {"Input an integer greater than zero"}
+        val factors = (1 until naturalNumber).filter { naturalNumber % it == 0 }
+        val sumOfFactors = factors.sum()
+        return when {
+            sumOfFactors < naturalNumber -> Classification.DEFICIENT
+            sumOfFactors == naturalNumber -> Classification.PERFECT
+            else -> Classification.ABUNDANT
+        }
+    }
+}
---
+++ perfect_numbers/src/test/kotlin/NaturalNumberTest.kt
@@ -0,0 +1,74 @@
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+import perfect_numbers.src.main.kotlin.Classification
+import perfect_numbers.src.main.kotlin.NaturalNumber
+
+class NaturalNumberTest {
+
+    @Test
+    fun smallPerfectNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.PERFECT, NaturalNumber.classify(6))
+    }
+
+    @Test
+    fun mediumPerfectNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.PERFECT, NaturalNumber.classify(28))
+    }
+
+    @Test
+    fun largePerfectNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.PERFECT, NaturalNumber.classify(33550336))
+    }
+
+    @Test
+    fun smallAbundantNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.ABUNDANT, NaturalNumber.classify(12))
+    }
+
+    @Test
+    fun mediumAbundantNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.ABUNDANT, NaturalNumber.classify(30))
+    }
+
+    @Test
+    fun largeAbundantNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.ABUNDANT, NaturalNumber.classify(33550335))
+    }
+
+    @Test
+    fun smallestPrimeDeficientNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.DEFICIENT, NaturalNumber.classify(2))
+    }
+
+    @Test
+    fun smallestNonPrimeDeficientNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.DEFICIENT, NaturalNumber.classify(4))
+    }
+
+    @Test
+    fun mediumNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.DEFICIENT, NaturalNumber.classify(32))
+    }
+
+    @Test
+    fun largeDeficientNumberIsClassifiedCorrectly() {
+        assertEquals(Classification.DEFICIENT, NaturalNumber.classify(33550337))
+    }
+
+    @Test
+    fun edgeCaseWithNoFactorsOtherThanItselfIsClassifiedCorrectly() {
+        assertEquals(Classification.DEFICIENT, NaturalNumber.classify(1))
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun zeroIsNotANaturalNumber() {
+        NaturalNumber.classify(0)
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun negativeNumberIsNotANaturalNumber() {
+        NaturalNumber.classify(-1)
+    }
+
+}
